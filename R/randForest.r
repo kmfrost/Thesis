3b6a370994d13d538d52ba3af663f75948943556
kmfrost <kmf0008@auburn.edu>
2016-05-04 00:13:30 -0400

    Worked through most of the small stuff today.  Gotta hit the big stuff tomorrow!

--- R/randForest.r
+++ R/randForest.r
@@ -1,4 +1,3 @@
-set.seed(100)
 
 setwd("D:/Documents/Github/Thesis/DataFiles")
 trai <- read.csv("trai_norm_email_features_35_30_35.csv")
@@ -11,8 +10,9 @@ rf <- randomForest(Status~., data=trai, ntree=500, sampsize=c(8,30,7,2,5,17), st
 pred <- predict(rf, test)
 confusionMatrix(pred, trai$Status)
 
-
-loocv_data <- read.csv("trai_norm_email_features_100.csv")
+set.seed(10)
+#loocv_data <- read.csv("trai_norm_email_features_100.csv")
+loocv_data <- read.csv("LOOCV_1.csv")
 loocv_data <- subset(loocv_data, select=-c(pid, Status))
-result <- rfcv(loocv_data, trai$Status, cv.fold=69,ntree=500, classwt=c(8,30,7,2,5,17))
+result <- rfcv(loocv_data, trai$Status, cv.fold=67,ntree=500, classwt=c(8,30,7,2,5,17))
 confusionMatrix(unlist(result$predicted['119']), trai$Status)
